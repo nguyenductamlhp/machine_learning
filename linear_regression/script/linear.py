8c93eaf829a328c2ad63da594638b00aa660c7d1
Nguyen Duc Tam <nguyenductamlhp@gmail.com>
2018-04-17 07:00:29 +0000

    [ADD] Nomalize data

--- linear_regression/script/linear.py
+++ linear_regression/script/linear.py
@@ -7,11 +7,25 @@ from numpy import set_printoptions
 
 def normalizeFeature(feature):
     '''
+    Normalize data
     '''
-    data = np.loadtxt('ex1data2.txt',skiprows=0, delimiter=',')
-    print data
-    
-normalizeFeature(1)
+    input_data = np.loadtxt('ex1data2.txt',skiprows=0, delimiter=',')
+    output = []
+    sums = np.sum(input_data, axis=0)
+    maxs = np.max(input_data, axis=0)
+    mins = np.min(input_data, axis=0)
+    for i in range(len(input_data[0])):
+        re = []
+        col = input_data[:, i]
+        delta = maxs[i] - mins[i]
+        avg = sums[i] / len(col)
+        for item in col:
+            re.append((1.0 * item - avg) / delta)
+        output.append(re)
+    return np.array(output)
+
+
+print normalizeFeature(1)
 
 # def computeCost():
 #     return 0
