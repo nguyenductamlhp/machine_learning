d48a73fca9daa8b0b64891476d0a0410cfaa30c1
Nguyen Duc Tam <nguyenductamlhp@gmail.com>
2018-04-19 16:29:00 +0000

    [ADD] Simple linear example

--- linear_regression/script/linear.py
+++ linear_regression/script/linear.py
@@ -1,15 +1,29 @@
 # -*- encoding: utf-8 -*-
 import numpy as np
 from pandas import read_csv
+import pandas as pd
 import os
 from sklearn import preprocessing
 from numpy import set_printoptions
+import matplotlib.pyplot as plt
 
-def normalizeFeature(feature):
+def getData(file_path):
+    """
+    (string) -> numpy array
+    Return numpy array ffrom file
+    """
+    dataFile = None
+    if os.path.exists(file_path):
+        dataFile = np.loadtxt(file_path, skiprows=0, delimiter=',')
+    else:
+        print "File not found!"
+    return dataFile
+
+def normalizeFeature(input_data):
     '''
-    Normalize data
+    (numpy array) -> numpy array
+    Return numpy array of data which is Normalized
     '''
-    input_data = np.loadtxt('ex1data2.txt',skiprows=0, delimiter=',')
     output = []
     sums = np.sum(input_data, axis=0)
     maxs = np.max(input_data, axis=0)
@@ -24,18 +38,36 @@ def normalizeFeature(feature):
         output.append(re)
     return np.array(output)
 
+def computeCost(theta_0, theta_1, theta_2, arr_x, arr_y):
+    sum = 0
+    for i in arr_x:
+        temp = (theta_0 + theta_1 * arr_x[i][0] + theta_2 * arr_x[i][1] - arr_y[i]) ** 2
+        sum = sum + temp
+    return sum * 1.0 / (2 * len(arr_x))
 
-print normalizeFeature(1)
+def computeGradient():
+    return 0
 
-# def computeCost():
-#     return 0
-    
 
-# def computeGradient():
-#     return 0
+
+# areas = [1000, 2000, 4000]
+# prices = [200000, 250000, 300000]
+# theta_0 = 0
+# testno = 200
+# costs = [computeSingleCost(theta_0, theta_1, areas, prices) for theta_1 in np.arange(testno)]
+# print ">>> costs", costs
+# plt.plot(np.arange(testno), costs)
+
+
 
 # def gradientDescent():
 #     return 0
 
 # def predict():
 #     return 0
+
+def main():
+    print normalizeFeature(getData('ex1data2.txt'))
+ 
+if __name__ == "__main__":
+    main()
