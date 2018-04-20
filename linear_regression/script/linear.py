fc3325a7201ab339b4f0f579b2d05aa360635daf
Nguyen Duc Tam <nguyenductamlhp@gmail.com>
2018-04-20 07:57:28 +0000

    [ADD] Complete multiple linear

--- linear_regression/script/linear.py
+++ linear_regression/script/linear.py
@@ -7,6 +7,8 @@ from sklearn import preprocessing
 from numpy import set_printoptions
 import matplotlib.pyplot as plt
 
+my_data = pd.read_csv('ex1data2.txt',names=["size","bedroom","price"])
+
 def getData(file_path):
     """
     (string) -> numpy array
@@ -38,36 +40,44 @@ def normalizeFeature(input_data):
         output.append(re)
     return np.array(output)
 
-def computeCost(theta_0, theta_1, theta_2, arr_x, arr_y):
-    sum = 0
-    for i in arr_x:
-        temp = (theta_0 + theta_1 * arr_x[i][0] + theta_2 * arr_x[i][1] - arr_y[i]) ** 2
-        sum = sum + temp
-    return sum * 1.0 / (2 * len(arr_x))
-
-def computeGradient():
-    return 0
-
-
-
-# areas = [1000, 2000, 4000]
-# prices = [200000, 250000, 300000]
-# theta_0 = 0
-# testno = 200
-# costs = [computeSingleCost(theta_0, theta_1, areas, prices) for theta_1 in np.arange(testno)]
-# print ">>> costs", costs
-# plt.plot(np.arange(testno), costs)
-
-
-
-# def gradientDescent():
-#     return 0
 
 # def predict():
 #     return 0
 
 def main():
-    print normalizeFeature(getData('ex1data2.txt'))
+    my_data = pd.read_csv('ex1data2.txt',names=["size","bedroom","price"])
+    my_data = (my_data - my_data.mean())/my_data.std()
+    print my_data.head()
+    #setting the matrixes
+    X = my_data.iloc[:,0:2]
+    ones = np.ones([X.shape[0],1])
+    X = np.concatenate((ones,X),axis=1)
+    
+    y = my_data.iloc[:,2:3].values #.values converts it from pandas.core.frame.DataFrame to numpy.ndarray
+    theta = np.zeros([1,3])
+    
+    #set hyper parameters
+    alpha = 0.01
+    iters = 1000
+    #computecost
+    def computeCost(X,y,theta):
+        tobesummed = np.power(((np.dot(X, theta.T))-y),2)
+        return np.sum(tobesummed)/(2 * len(X))
+    
+    #gradient descent
+    def gradientDescent(X,y,theta,iters,alpha):
+        cost = np.zeros(iters)
+        for i in range(iters):
+            theta = theta - (alpha/len(X)) * np.sum(X * (np.dot(X, theta.T) - y), axis=0)
+            cost[i] = computeCost(X, y, theta)
+        
+        return theta,cost
+    #running the gd and cost function
+    g,cost = gradientDescent(X,y,theta,iters,alpha)
+    print(">>>g", g)
+    
+    finalCost = computeCost(X,y,g)
+    print(">>> finalCost", finalCost)
  
 if __name__ == "__main__":
     main()
