6e746e3ca257fed4ba9ebee16d68c3f363bb9249
Nguyen Duc Tam <nguyenductamlhp@gmail.com>
2018-04-22 08:22:41 +0000

    [ADD] Human class

---
+++ python_excercise/excercise2/1312509_human.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+import sys
+from datetime import datetime
+import re
+import json
+
+
+class Human:
+
+    def __init__(self, name=None, birthyear=None):
+        self.name = name
+        self.birthyear = birthyear
+
+    def read_from_json(self, file_name):
+        data = json.load(open(file_name))
+        self.name = data['Name']
+        self.birthyear = data['BirthYear']
+
+    def get_age(self):
+        td = datetime.now().date()
+        cur_year = td.year
+        return cur_year - self.birthyear
+
+    def show_info(self):
+        print "Name: ", self.name
+        print "Birth Year: ", self.birthyear
+        print "Age: ", self.get_age()
+        
+    
+def main(argv):
+    human = Human(None, None)
+    human.read_from_json(argv[1])
+    human.show_info()
+
+if __name__ == "__main__":
+    print "Read and display human infomation ..."
+    main(sys.argv[:])
